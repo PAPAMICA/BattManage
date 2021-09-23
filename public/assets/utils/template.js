fc5abf57ab59234fff4d51ecc735138baabfe3b4
Mickael Asseline <mickael@papamica.com>
2021-09-23 13:40:40 +0200

    Change title

--- public/assets/utils/template.js
+++ public/assets/utils/template.js
@@ -5,7 +5,7 @@ const template = (data) => (
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Table - Brand</title>
+    <title>BattManage | <%= battery.Name %></title>
     <link rel="icon" type="image/png" sizes="50x50" href="../assets/img/logo-batmanage.png">
     <link rel="icon" type="image/png" sizes="50x50" href="../assets/img/logo-batmanage.png">
     <link rel="icon" type="image/png" sizes="50x50" href="../assets/img/logo-batmanage.png">
